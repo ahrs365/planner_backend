6ec2d75e3da923966849b014be36cee706a96176
ahrs <ahrs365@outlook.com>
2026-01-16 18:19:06 +0800

    Remove build from git tracking

--- build/src/tmpc/ros_tools_no_ros/CMakeFiles/ros_tools_no_ros.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libros_tools_no_ros.a"
-)
--- build/third_party_yaml_cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gao/workspace/pnc_project/web_robot/backend/third_party/yaml-cpp
-# Build directory: /home/gao/workspace/pnc_project/web_robot/backend/build/third_party_yaml_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
