86bc0a5ade3310dae6a587caf68c37a4e98e3f90
ahrs <ahrs365@outlook.com>
2026-01-21 16:13:32 +0800

    修复acados报错问题

--- src/tmpc/mpc_planner_solver/solver.cmake
+++ src/tmpc/mpc_planner_solver/solver.cmake
@@ -1,12 +1,10 @@
 add_definitions(-DACADOS_SOLVER)
-string(REPLACE ":" ";" LIBRARY_DIRS $ENV{LD_LIBRARY_PATH})
-find_library(acados_LIBRARY libacados.so PATHS ${LIBRARY_DIRS})
-find_library(blasfeo_LIBRARY libblasfeo.so PATHS ${LIBRARY_DIRS})
-find_library(hpipm_LIBRARY libhpipm.so PATHS ${LIBRARY_DIRS})
-# Get the acados path from the LD_LIBRARY_PATH
-get_filename_component(acados_path ${acados_LIBRARY} DIRECTORY)
-set(acados_path ${acados_path}/..)
-set(acados_include_path ${acados_path}/include)
+set(ACADOS_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../..//third_party/acados")
+set(ACADOS_LIB_DIR "${ACADOS_SOURCE_DIR}/lib")
+find_library(acados_LIBRARY libacados.so PATHS ${ACADOS_LIB_DIR} NO_DEFAULT_PATH)
+find_library(blasfeo_LIBRARY libblasfeo.so PATHS ${ACADOS_LIB_DIR} NO_DEFAULT_PATH)
+find_library(hpipm_LIBRARY libhpipm.so PATHS ${ACADOS_LIB_DIR} NO_DEFAULT_PATH)
+set(acados_include_path ${ACADOS_SOURCE_DIR}/include)
 # Print acados_include_path
 set(solver_LIBRARIES
     ${PROJECT_SOURCE_DIR}/acados/Solver/libacados_ocp_solver_Solver.so # Generated files
